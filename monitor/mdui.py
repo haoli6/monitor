c2c6871d5d9cb6ea051959f95ad8af36d73b4049
haoli6 <104366822+haoli6@users.noreply.github.com>
2024-07-08 08:56:25 +0800

    Update mdui.py

--- monitor/mdui.py
+++ monitor/mdui.py
@@ -7,7 +7,7 @@ class Ui_Dialog(object):
         Dialog.resize(1377, 817)
         Dialog.setStyleSheet("")
 
-        # Title
+        # 标题
         self.title = QtWidgets.QLabel(Dialog)
         self.title.setGeometry(QtCore.QRect(290, 80, 751, 71))
         font = QtGui.QFont()
@@ -18,20 +18,20 @@ class Ui_Dialog(object):
         self.title.setAlignment(QtCore.Qt.AlignCenter)
         self.title.setObjectName("title")
 
-        # Username Input
+        # 用户名输入
         self.username = QtWidgets.QLineEdit(Dialog)
         self.username.setGeometry(QtCore.QRect(500, 300, 361, 41))
         self.username.setPlaceholderText("请输入用户名")
         self.username.setObjectName("username")
 
-        # Password Input
+        # 密码输入
         self.password = QtWidgets.QLineEdit(Dialog)
         self.password.setGeometry(QtCore.QRect(500, 360, 361, 41))
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.password.setPlaceholderText("请输入密码")
         self.password.setObjectName("password")
 
-        # Login Button
+        # 登录按钮
         self.go = QtWidgets.QPushButton(Dialog)
         self.go.setGeometry(QtCore.QRect(570, 610, 211, 61))
         font = QtGui.QFont()
@@ -42,7 +42,7 @@ class Ui_Dialog(object):
             "\nQPushButton\n{\n    background-color: #473C8B;\n    border-style: outset;\n    border-width: 2px;\n    border-radius:5px;\n    border-color: #8B7355;\n    color:white;\n    font: 22pt \"华文新魏\";\n    padding: 5px;\n}\nQPushButton:hover\n{\n    background-color: rgb(0, 150, 0);\n}\nQPushButton:pressed\n{\n    background-color: #FF6A6AF;\n    border-style: inset;\n}\nQPushButton:!enabled{\n    background-color: rgb(100, 100, 100);\n    border-style: inset;\n}")
         self.go.setObjectName("go")
 
-        # Register Button
+        # 注册按钮
         self.register = QtWidgets.QPushButton(Dialog)
         self.register.setGeometry(QtCore.QRect(570, 690, 211, 61))
         font = QtGui.QFont()
@@ -54,8 +54,8 @@ class Ui_Dialog(object):
         self.register.setObjectName("register")
 
         self.retranslateUi(Dialog)
-        self.go.clicked.connect(Dialog.goin)  # type: ignore
-        self.register.clicked.connect(Dialog.register_user)  # type: ignore
+        self.go.clicked.connect(Dialog.goin)  
+        self.register.clicked.connect(Dialog.register_user)  
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
